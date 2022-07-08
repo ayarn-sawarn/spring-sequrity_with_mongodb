116a57cb7fad30de3bf8f38a73ea3a05e503a043
ayarn-sawarn <ayarn.sawarn@oodles.io>
2022-07-08 21:35:13 +0530

    project setup

---
+++ src/main/java/com/securitywithmongodb/SecuremongodbApplication.java
@@ -0,0 +1,13 @@
+package com.securitywithmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuremongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuremongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/securitywithmongodb/SecuremongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securitywithmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuremongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
